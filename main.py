333725c6e197b91baf6983428ec8e7b40dd051ac
justjo <johnpjust@gmail.com>
2019-10-01 14:52:02 -0500

    update

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from scipy import ndimage as ndi
 import cv2
 cmap = matplotlib.colors.ListedColormap (np.random.rand ( 256,3))
 
-img = io.imread(r'D:\Matlab\GrainParticleSize\NG3_GQ_Corn_11MC_59lbs_50F_2017-11-16_11-0-33_Sensor-1_Frame-36_Ts-1510851850.1548.png')
-# img = io.imread(r'D:\Matlab\GrainParticleSize\NG3_GQ_Corn_32MC_53lbs_99F_2017-7-18_10-31-45_Sensor-1_Frame-32_Ts-1500374550.1573.png')
+# img = io.imread(r'D:\Matlab\GrainParticleSize\NG3_GQ_Corn_11MC_59lbs_50F_2017-11-16_11-0-33_Sensor-1_Frame-36_Ts-1510851850.1548.png')
+img = io.imread(r'D:\Matlab\GrainParticleSize\NG3_GQ_Corn_32MC_53lbs_99F_2017-7-18_10-31-45_Sensor-1_Frame-32_Ts-1500374550.1573.png')
 
 img = img/255.;
 x1 = 63;x2 = x1+597;y1 = 21;y2 = y1+439;
@@ -24,8 +24,13 @@ imgc = imgc_-np.expand_dims(background, axis=2);imgc = np.minimum(imgc - np.min(
 ## could do this after finding sure foreground as well so that we can ensure the watershed is only used to separate the kernels
 # imgca = exposure.equalize_adapthist(imgc)
 
+##### hyperparameters include the disk size, the distance-transform threshold, and the gradient lines stdev ######
+dr = 20
+dt_t = 5
+grad_t = 4
+
 ## sure background
-se = morphology.disk(20);
+se = morphology.disk(dr);
 Ie = morphology.erosion(color.rgb2gray(imgc),se);
 Iobr = morphology.reconstruction(Ie,color.rgb2gray(imgc))
 thresh = filters.threshold_otsu(Iobr)
@@ -36,9 +41,9 @@ bg = bg.astype(np.float)
 
 ## subtract off gradient lines from sure_bg then find markers
 sure_bg=np.max(bg, axis=2)
-gradlines = feature.canny(color.rgb2gray(imgc), 4)
+gradlines = feature.canny(color.rgb2gray(imgc), grad_t)
 D1 = ndi.distance_transform_edt(np.maximum(sure_bg-gradlines,0))
-dist_binary1 = D1 > 5
+dist_binary1 = D1 > dt_t
 dist_binary1 = dist_binary1.astype(np.float)
 # plt.figure();plt.imshow(dist_binary1)
 dist_binary1_ = morphology.remove_small_holes(util.pad(dist_binary1, (1,), 'constant', constant_values=0).astype(np.int), 400)
@@ -47,7 +52,7 @@ dist_binary1_ = dist_binary1_[1:-1, 1:-1]
 ## sure foreground (use only 2nd and 3rd channels and find markers)
 fat_markers=bg[:,:,1]
 D2 = ndi.distance_transform_edt(fat_markers)
-dist_binary2 = D2 > 5
+dist_binary2 = D2 > dt_t
 dist_binary2 = dist_binary2.astype(np.float)
 # plt.figure();plt.imshow(dist_binary2)
 dist_binary2_ = morphology.remove_small_holes(util.pad(dist_binary2, (1,), 'constant', constant_values=0).astype(np.int), 300)
